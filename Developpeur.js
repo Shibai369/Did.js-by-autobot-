1dce200eba81e97ba0720698381e5205a7960e78
Shibai Otsutsuki <shibaiotsutsuki83@gmail.com>
2024-09-30 13:55:05 +0000

    Update Developpeur.js

--- Developpeur.js
+++ Developpeur.js
@@ -27,7 +27,7 @@ module.exports.run = async function({ api, event, args }) {
 		const prefix = "🤣";
 		const CREATORLINK = "https://www.facebook.com/profile.php?id=61566021226987";
 		const BOTCREATOR = "Did.js Officiel";
-		const BOTNAME = "MEGAN AE";
+		const BOTNAME = "Did.js Officiel";
 		const FILESOWNER = "AE";
 		const juswa = moment.tz("Asia/Manila").format("『D/MM/YYYY』 【HH:mm:ss】");
 		const link = ["https://i.imgur.com/9LDVC57.mp4", "https://i.imgur.com/r7IxgiR.mp4", "https://i.imgur.com/J1jWubu.mp4", "https://i.imgur.com/DJylTiy.mp4", "https://i.imgur.com/v4mLGte.mp4", "https://i.imgur.com/uthREbe.mp4", "https://i.imgur.com/ee8fHna.mp4", "https://i.imgur.com/VffzOwS.mp4", "https://i.imgur.com/ci5nztg.mp4", "https://i.imgur.com/qHPeKDV.mp4", "https://i.imgur.com/Rkl5UmH.mp4",
